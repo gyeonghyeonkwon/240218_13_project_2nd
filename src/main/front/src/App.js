7a97cefc510d932f99b6bc00e3e4591f75b3b51a
gyeonghyeonkwon <kyanghyang12@naver.com>
2024-02-15 22:20:32 +0900

    react start

---
+++ src/main/front/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './index.css';
+import root from './router/root';
+
+function App() {
+  return (
+    <RouterProvider router={root}></RouterProvider>//라우터 , root.js
+
+  );
+}
+
+export default App;
---
+++ src/main/front/src/router/root.js
@@ -0,0 +1,46 @@
+import { Suspense, lazy } from "react";
+const { createBrowserRouter, Navigate } = require("react-router-dom");
+
+const Loading = <span className="loading loading-spinner loading-lg"></span>//로딩
+
+const Login = lazy(() => import("../pages/member/LoginPage")) //보여질 로그인 페이지 
+const PostList = lazy(() => import("../post/list/PostList")) //보여질 리스트 페이지 
+const Write = lazy(() => import("../post/create/Write")) //보여질 글작성 페이지 
+const PostModify = lazy(() => import("../post/modify/PostModify"))
+const Detail = lazy(() => import("../post/detail/Detail"))
+
+
+
+const root = createBrowserRouter ([
+  
+  {
+    path: '',
+    element: <Navigate replace={true} to= {'post/list'}/> //리다이렉션
+  },
+  {
+    path: "member/login", //로그인 페이지 이동 
+    element: <Suspense fallback={Loading}><Login/></Suspense>
+  },
+  {
+    path: "post/list", //글 리스트 이동 
+    element: <Suspense fallback={Loading}><PostList/></Suspense>
+  },
+  {
+    path: "post/write", //글 작성 이동
+    element: <Suspense fallback={Loading}><Write/></Suspense>
+  },
+  {
+    path: 'post/modify/:id', //글 수정 이동
+    element: <Suspense fallback={Loading}><PostModify/></Suspense>
+    
+  },
+  {
+    path: 'post/detail/:id', //글 상세 이동 
+    element: <Suspense fallback={Loading}><Detail/></Suspense>
+    
+  }
+
+
+])
+
+export default root;
